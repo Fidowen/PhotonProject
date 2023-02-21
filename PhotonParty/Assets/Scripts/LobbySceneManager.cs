b20f94fb4b3e376317c3f98f3e412455c7b2925f
your name <username@emailpath.com>
2023-02-21 20:07:12 +0800

    Ch2 end

--- PhotonParty/Assets/Scripts/LobbySceneManager.cs
+++ PhotonParty/Assets/Scripts/LobbySceneManager.cs
@@ -5,11 +5,14 @@ using Photon.Pun;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Photon.Realtime;
+using System.Text;
 
 public class LobbySceneManager : MonoBehaviourPunCallbacks
 {
     [SerializeField]
     InputField inputRoomName;
+    [SerializeField]
+    Text textRoomList;
 
     void Start()
     {
@@ -43,6 +46,19 @@ public class LobbySceneManager : MonoBehaviourPunCallbacks
             print("Invaild RoomName");
         }
     }
+
+    public void OnClickJoinRoom()
+    {
+        string roomName = GetRoomName();
+        if (roomName.Length > 0)
+        {
+            PhotonNetwork.JoinRoom(roomName);
+        }
+        else
+        {
+            print("Invaild RoomName!");
+        }
+    }
     public override void OnJoinedRoom()
     {
         print("Room Joined!");
@@ -50,6 +66,16 @@ public class LobbySceneManager : MonoBehaviourPunCallbacks
     }
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
-        base.OnRoomListUpdate(roomList);
+        print("update");
+        StringBuilder sb = new StringBuilder();
+        foreach (RoomInfo roomInfo in roomList)
+        {
+            print(roomInfo.PlayerCount);
+            if (roomInfo.PlayerCount > 0)
+            {
+                sb.AppendLine(">" + roomInfo.Name + roomInfo.PlayerCount);
+            }
+        }
+        textRoomList.text = sb.ToString();
     }
 }
